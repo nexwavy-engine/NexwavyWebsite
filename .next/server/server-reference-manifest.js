3437425bfa9383f6968253725e482a6f5cf11dff
Optimize Geek <optimization.ng@sunking.com>
2026-07-28 20:38:32 +0100

    fix(vercel): remove invalid rootDirectory from vercel.json

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"J8fmLZOt2Wp5WC6s2vPEy8mE6GweP2PIgN6XLxnKbrA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"YNfigjJl1XqAziTItszsvaLy6+nQJuuzS35aNAGvexM=\"}"
